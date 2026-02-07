9fe65677758d135d0692c8d18dae52590f0667bf
Laharish01 <laharish2k17@gmail.com>
2026-02-07 18:24:32 +0530

    pathname

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools()
   ],
-  base: "laharish01.github.io",
+  base: "texthooker",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
